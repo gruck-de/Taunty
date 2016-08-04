a2d3afd891f680e33a01393961e93b0caf964a03
gruck-de <christian.czayka@gmail.com>
2016-08-04 11:13:13 +0200

    Update taunty.lua

--- taunty.lua
+++ taunty.lua
@@ -4,3 +4,17 @@ function taunty:onInit()
   print("Taunty watching over you...");
   self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 end
+
+
+function addon:COMBAT_LOG_EVENT_UNFILTERED(timestamp, subevent, hideCaster, ...)
+  local spellID, spellname, spellschool, 
+     extraspellID
+  srcGUID, srcname, srcflags, srcRaidFlags,
+  dstGUID, dstname, dstflags, dstRaidFlags,
+  spellID, spellname, spellschool, 
+  extraspellID = ...
+  
+  if (subevent == "SPELL_CAST_SUCCESS")
+    print("Player casted: " .. (spellname or "nil"));
+  end
+end
