1a507f875d3b44f80e54b4e1e8d6ee458bfa81ec
gruck-de <christian.czayka@gmail.com>
2016-08-04 11:29:54 +0200

    Update taunty.lua

--- taunty.lua
+++ taunty.lua
@@ -1,10 +1,18 @@
 local taunty = taunty;
+local tauntSpellNames = fillTaunts("355", "62124", "116189"); -- tbd (Warr, Pala, Monk)
 
 function taunty:onInit()
   print("Taunty watching over you...");
   self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 end
 
+function fillTaunts(spellIDs)
+  local result = {};
+  for i, v in ipairs(spellIDs) do
+    local spellName = GetSpellInfo(v); -- Hier könnte ein Fehler auftreten wenn es die ID nicht gibt
+    result[spellName] = true;
+  end
+end
 
 function addon:COMBAT_LOG_EVENT_UNFILTERED(timestamp, subevent, hideCaster, ...)
   local spellID, spellname, spellschool, 
@@ -15,6 +23,10 @@ function addon:COMBAT_LOG_EVENT_UNFILTERED(timestamp, subevent, hideCaster, ...)
   extraspellID = ...
   
   if (subevent == "SPELL_CAST_SUCCESS")
-    print("Player casted: " .. (spellname or "nil"));
+    if (tauntSpellNames[spellname])
+        print("Player taunted: " .. dstname .. (" with ") .. (spellname or "nil"));
+    elseif
+      print("Player casted: " .. (spellname or "nil"));
+    end
   end
 end
