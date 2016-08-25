1e4b77b964bddfea781e86b73e7f024af4c4bda3
gruck-de <mail@gruck.de>
2016-08-25 16:47:52 +0200

    Make it nice and quiet. Don't bother with "outside"-taunts

--- taunty.lua
+++ taunty.lua
@@ -68,18 +68,18 @@ local raidIconLookup = {
     [COMBATLOG_OBJECT_RAIDTARGET8] = 8,
 }
 
-local scanTool = CreateFrame( "GameTooltip", "ScanTooltip", nil, "GameTooltipTemplate" )
-scanTool:SetOwner( WorldFrame, "ANCHOR_NONE" )
-local scanText = _G["ScanTooltipTextLeft2"] -- This is the line with <[Player]'s Pet>
+local scanTool = CreateFrame("GameTooltip", "ScanTooltip", nil, "GameTooltipTemplate")
+scanTool:SetOwner(WorldFrame, "ANCHOR_NONE")
+local scanText = _G["ScanTooltipTextLeft2"]-- This is the line with <[Player]'s Pet>
 
 function getPetOwner(petName)
-   scanTool:ClearLines()
-   scanTool:SetUnit(petName)
-   local ownerText = scanText:GetText()
-   if not ownerText then return nil end
-   local owner, _ = string.split("'",ownerText)
-   
-   return owner -- This is the pet's owner
+    scanTool:ClearLines()
+    scanTool:SetUnit(petName)
+    local ownerText = scanText:GetText()
+    if not ownerText then return nil end
+    local owner, _ = string.split("'", ownerText)
+    
+    return owner -- This is the pet's owner
 end
 
 
@@ -123,26 +123,46 @@ function Taunty:COMBAT_LOG_EVENT_UNFILTERED(...)
         
         -- check if pvp and return if true
         if UnitInBattleground('player') or GetZonePVPInfo() == 'arena' then return end
-
-
-if debug then        
- Taunty:sendMsg(srcGUID);
- Taunty:sendMsg(srcname);
- Taunty:sendMsg(getPetOwner(srcname));
- Taunty:sendMsg(srcflags);
- end
-
-
+        
+        
+        if debug then
+            Taunty:sendMsg(srcGUID);
+            Taunty:sendMsg(srcname);
+            Taunty:sendMsg(getPetOwner(srcname));
+            Taunty:sendMsg(srcflags);
+        end
+        
+        
         if (srcGUID == playerid) then -- player taunt
             Taunty:sendMsg(("%s %s taunted %s with %s"):format(GetSpecializationRoleByID(GetInspectSpecialization(srcname)), srcname, dstname, GetSpellLink(spellID)));
             PlaySoundFile("Sound\\interface\\PickUp\\PickUpMetalSmall.ogg", "Master");
         else
+            local inScope = false;
+            
+            -- check if in my raid
+            if bit.band(srcflags, COMBATLOG_OBJECT_AFFILIATION_RAID) > 0 then
+                inScope = true;
+            end
+            
+            -- check if in my group
+            if bit.band(srcflags, COMBATLOG_OBJECT_AFFILIATION_PARTY) > 0 then
+                inScope = true;
+            end
+            
+            -- check if is mine
+            if bit.band(srcflags, COMBATLOG_OBJECT_AFFILIATION_MINE) > 0 then
+                inScope = true;
+            end
+            
+            -- not in scope, so we don't want to know
+            if not inScope then return end
+            
             -- check if it was a pet
             if bit.band(srcflags, COMBATLOG_OBJECT_CONTROL_PLAYER) > 0 then
                 local owner = getPetOwner(srcname);
-                if owner then               
+                if owner then
                     srcname = srcname .. " <" .. owner .. ">";
-                end               
+                end
             else
                 -- since it's not a pet, it could have a role
                 local whatRole = GetSpecializationRoleByID(GetInspectSpecialization(srcname));
@@ -150,25 +170,22 @@ if debug then
                     srcname = whatRole .. srcname;
                 end
             end
+            
+            -- prevent error due to Earth Elemental taunt
+            if dstname == nil then
+                dstname = "UNKNOWN";
+            end
 
-
-            -- check if source in in my group
-            --if (raidonly and bit.band(srcflags, COMBATLOG_OBJECT_AFFILIATION_RAID) > 0) or (grouponly and bit.band(srcflags, COMBATLOG_OBJECT_AFFILIATION_PARTY) > 0) then
- 
-                -- prevent error due to Earth Elemental taunt
-                if dstname == nil then
-                    dstname = "UNKNOWN";
-                end
-                playerid = UnitGUID("player");
-                
-                if (UnitGUID("targettarget") == playerid) then -- player is the target
-                    Taunty:sendMsg(("%s ninja-taunted %s with %s"):format(srcname, dstname, GetSpellLink(spellID)));
-                    PlaySoundFile("Sound\\Doodad\\G_NecropolisWound.ogg", "Master");
-                else
-                    Taunty:sendMsg(("%s taunted %s with %s"):format(srcname, dstname, GetSpellLink(spellID)));
-                    PlaySoundFile("Sound\\interface\\PickUp\\PickUpMetalSmall.ogg", "Master");
-                end
-            --end
+            playerid = UnitGUID("player");
+            
+            if (UnitGUID("targettarget") == playerid) then -- player is the target
+                Taunty:sendMsg(("%s ninja-taunted %s with %s"):format(srcname, dstname, GetSpellLink(spellID)));
+                PlaySoundFile("Sound\\Doodad\\G_NecropolisWound.ogg", "Master");
+            else
+                Taunty:sendMsg(("%s taunted %s with %s"):format(srcname, dstname, GetSpellLink(spellID)));
+                PlaySoundFile("Sound\\interface\\PickUp\\PickUpMetalSmall.ogg", "Master");
+            end
+        --end
         end
     elseif (subevent == "SPELL_AURA_APPLIED") and (aeotauntSpellNames[spellname]) then
         -- check if pvp and return if true
@@ -179,7 +196,7 @@ if debug then
     elseif (subevent == "SPELL_MISSED") and (tauntSpellNames[spellname] or aeotauntSpellNames[spellname]) then
         -- check if pvp and return if true
         if UnitInBattleground('player') or GetZonePVPInfo() == 'arena' then return end
-
+        
         Taunty:sendMsg(("%s taunt failed on %s with %s. Reason: %s"):format(srcname, dstname, GetSpellLink(spellID), misstype));
         PlaySoundFile("Sound\\Spells\\SimonGame_Visual_GameFailedSmall.ogg", "Master");
     end
